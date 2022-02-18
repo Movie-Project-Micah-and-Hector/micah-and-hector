e0fa4b60c142f1b61886951df65e621787ed87da
Micah Wood <micahswood7@gmail.com>
2022-02-18 16:13:06 -0600

    massive progress in movie-project

--- movie-script.js
+++ movie-script.js
@@ -10,25 +10,34 @@ function getAllMovies() {
 }
 getAllMovies();
 
-function addingMovies() {
-    const addMovie = {
+function addMovies() {
+    const addition = {
         title: 'Night of the Living Hector',
         rating: 5,
-        genre: 'Nature, Documentary',
-        id: 6
+        genre: 'Nature, Documentary'
     };
     const option = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(addMovie),
+        body: JSON.stringify(addition),
     };
     fetch(url, option)
+        .then( response => console.log(response))
+        .catch( error => console.error(error));
+}
+// addMovies();
+
+function deletingMovies(id) {
+    const option = {
+        method: `DELETE`
+    }
+    fetch(`${url}/${id}`, option)
         .then( response => console.log(response))
         .then(option => console.log(option))
         .catch( error => console.error(error));
 }
-addingMovies();
-
+// deletingMovies()
+// deleted empty: 6,7,8,9,10,11,12,13,14,15,16,256,257,258,259,260,261
 
