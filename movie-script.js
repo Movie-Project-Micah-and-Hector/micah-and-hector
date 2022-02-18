5ef761fae4a059c76038a7f8cd0825a4edac84cb
Micah Wood <micahswood7@gmail.com>
2022-02-18 15:40:54 -0600

    adding to movie-project

--- movie-script.js
+++ movie-script.js
@@ -2,17 +2,33 @@
 
 const url = 'https://agreeable-sumptuous-lilac.glitch.me/movies'
 
-let option = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(url)
+function getAllMovies() {
+    fetch(url)
+        .then(response => response.json())
+        .then(movies => console.log(movies))
+        .catch(error => console.error(error));
 }
+getAllMovies();
 
-fetch(url, option)
-    .then(response => response.json())
-    .catch( error => console.error(error) );
-console.log(url)
+function addingMovies() {
+    const addMovie = {
+        title: 'Night of the Living Hector',
+        rating: 5,
+        genre: 'Nature, Documentary',
+        id: 6
+    };
+    const option = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(addMovie),
+    };
+    fetch(url, option)
+        .then( response => console.log(response))
+        .then(option => console.log(option))
+        .catch( error => console.error(error));
+}
+addingMovies();
 
 
